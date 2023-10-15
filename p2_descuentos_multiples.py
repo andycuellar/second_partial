87348eb88c282b3987ddcb2db8022a5eaceb0728
andycuellar <145025718+andycuellar@users.noreply.github.com>
2023-10-14 22:00:19 -0600

    p2_descuentos_multiples.py

---
+++ p2_descuentos_multiples.py
@@ -0,0 +1,16 @@
+#Compra con descuentos múltiples
+precio=float(input("Hola, buen día! Por favor ingresa el precio del producto:"))
+unidades=float(input("Ingresa la cantidad de unidades:"))
+categoria=input("Ahora ingresa la categoría: A, B o C: ")
+if categoria=='A':
+  precio=int(precio)-int(precio)*.10
+  print("Tu precio con descuento es", precio)
+elif categoria=='B':
+  precio=int(precio)-int(precio)*.05
+  print("Tu precio con descuento es", precio)
+elif categoria=='C':
+  precio=int(precio)-int(precio)*.02
+  print("Tu precio con descuento es", precio)
+if unidades>=10:
+  extra=0.5
+  print("Obtienes un descuento adicional del 5%", int(precio)-int(precio)*.05)
