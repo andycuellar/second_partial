ec4ca9345e0dec7b4669af3bd3ff474bad8d75d0
andycuellar <145025718+andycuellar@users.noreply.github.com>
2023-10-14 22:59:34 -0600

    p2_ahorro_jubilacion.py

---
+++ p2_ahorro_jubilacion.py
@@ -0,0 +1,10 @@
+edad=float(input("Ingrese su edad actual:"))
+jubilacion=float(input("Ingrese la edad a la que planea jubilarse:"))
+cantidad=float(input("Ingrese la cantidad deseada para su jubilación:"))
+r=0.25/12
+n=(jubilacion-edad)*12
+t=jubilacion-edad
+numero=jubilacion*r
+funcion=((1+r)**n-1*(1+r)**(-t))
+pmt=numero/funcion
+print("El pago mensual para la jubilación es de", pmt)
