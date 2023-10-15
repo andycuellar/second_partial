5e30141099a8cb15b87511f73f875aa00e7151a3
andycuellar <145025718+andycuellar@users.noreply.github.com>
2023-10-14 22:34:34 -0600

    p2_calculadora_calorias.py

---
+++ p2_calculadora_calorias.py
@@ -0,0 +1,18 @@
+peso=float(input("Ingresa tu peso en kilogramos:"))
+duracion=float(input("Ingresa la duración en minutos:"))
+actividad=input("Ingresa el tipo de actividad (correr, nadar, caminar, ciclismo, levantar pesas):")
+if actividad=='correr':
+  calorias=9.8*peso*duracion
+  print("La cantidad de calorías quemadas es", calorias)
+elif actividad=='nadar':
+  calorias=7*peso*duracion/200
+  print("La cantidad de calorías quemadas es", calorias)
+elif actividad=='ciclismo':
+  calorias=6*peso*duracion/200
+  print("La cantidad de calorías quemadas es", calorias)
+elif actividad=='caminar':
+  calorias=2.9*peso*duracion/200
+  print("La cantidad de calorías quemadas es", calorias)
+elif actividad=='levanta pesas':
+  calorias=3*peso*duracion/200
+  print("La cantidad de calorías quemadas es", calorias)
