db5a7e83858944ed4e7d9a2a3cef57827557e90e
andycuellar <145025718+andycuellar@users.noreply.github.com>
2023-10-14 23:48:30 -0600

    Create p2_plan_carretera.py

---
+++ p2_plan_carretera.py
@@ -0,0 +1,6 @@
+millas=float(input("Ingresa la distancia que recorres en millas:"))
+millasxgalon=float(input("Ingresa el rendimiento de millas por galón:"))
+precio=float(input("Ingresa el precio actual de la gasolina:"))
+dias=input("¿Cuántos días vas a viajar?:")
+costo=(millas/millasxgalon)*precio*int(dias)
+print("El precio por viajar esa distancia es de", costo)
